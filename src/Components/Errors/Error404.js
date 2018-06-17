e8838820ee66e6d6673283dca6521052e1dce70e
elorsborn <elorsborn17@gmail.com>
2018-06-17 17:44:13 +0100

    Error page done

--- src/Components/Errors/Error404.js
+++ src/Components/Errors/Error404.js
@@ -1,11 +1,25 @@
 import React from "react";
+import "./Error404.css";
 
 const Error404 = props => {
-  console.log(props);
   if (props.match.path === "/articles/404")
-    return <div>'No such article exists!'</div>;
+    return (
+      <div className="Error-container">
+        <div className="Error-title">404</div>
+        <p className="Error-message">
+          Sorry, we can't find the page you're looking for.
+        </p>
+      </div>
+    );
   if (props.match.path === "/topics/:topic/404")
-    return <div>No such topic exists!</div>;
+    return (
+      <div className="Error-container">
+        <div className="Error-title">404</div>
+        <p className="Error-message">
+          Sorry, we can't find the page you're looking for.
+        </p>
+      </div>
+    );
 };
 
 Error404.prototypes = {};
